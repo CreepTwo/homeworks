0386d7eeebd052c234e03fe2d2dfba0be85ea786
Oleg <orlovoleg928@gmail.com>
2020-04-24 16:43:41 +0300

    24 april

--- yoga/js/script.js
+++ yoga/js/script.js
@@ -108,7 +108,136 @@ window.addEventListener('DOMContentLoaded', function() {
         document.body.style.overflow = '';
     });
 
+    // Form
+
+    let message = {
+        loading: 'Загрузка...',
+        success: 'Спасибо! Мы свяжемся с вами!',
+        failure: 'Упс! Что-то пошло не так...'
+    };
+
+    let form = document.querySelector('.main-form'),
+        input = form.getElementsByTagName('input'),
+        statusMessage = document.createElement('div');
+
+        statusMessage.classList.add('status');
+
+        function sendForm(elem) {
+            elem.addEventListener('submit', function(e) {
+                e.preventDefault();
+                    elem.appendChild();
+                    let formData = new FormData(elem);
+    
+                    function postData(data) {
+                        return new Promise(function(resolve,reject) {
+                            let request = new XMLHttpRequest();
+                            request.open('POST', 'server.php');
+                            request.setRequestHeader('Content-Type', 'application/json; charset=utf-8');
+
+                            request.onreadystatechange = function () {
+                                if(request.readyState < 4) {
+                                    resolve()
+                                }else if (request/this.readyState === 4) {
+                                    if(request.status == 200 && request.status < 3){
+                                        resolve()
+                                    }
+                                    else{
+                                        reject()
+                                    }
+                                }
+                            }
+
+                            request.send(data);
+                        })
+                    } //End postData
+
+                    function clearInput() {
+                        for(let i = 0; i < input.length; i++) {
+                            input[i].value = '';
+                        }
+                    }
+
+                    postData(formData)
+                         .then(() => statusMessage.innerHTML = message.loading)
+                         .then(()=> {
+                             thanksModal.style.display = 'block';
+                             mainModal.style.display = 'none';
+                             statusMessage.innerHTML = '';
+                         })
+                         .catch(() => statusMessage.innerHTML = message.failure)
+                         .then(clearInput)
+            });
+        }
+
+        sendForm(form);
+        sendForm(formBottom);
+    
+
 });
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//     // Options
+
+//     class Options {
+//         constructor(height, width, bg, fontSize, textAlign) {
+//             this.height = height;
+//             this.width = width;
+//             this.bg = bg;
+//             this.fontSize = fontSize;
+//             this.textAlign = textAlign;
+//         }
+
+//         createDiv() {
+//             let elem = document.createElement('div');
+//             document.body.appendChild(elem);
+//             let param = `height:${this.height}px; width:${this.width}px; background-color:${this.bg}; font-size:${this.fontSize}px; text-align:${this.textAlign}`;
+//             elem.style.cssText = param;
+//         }
+//     }
+
+//     const item = new Options(300, 350, "red", 14, "center");
+//     item.createDiv();
+
+// 
+
+
 
 // let age = document.getElementById('age');
  
