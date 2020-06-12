36603a98f8322e0f15e4a7b7229d22e087f65da5
Oleg <orlovoleg928@gmail.com>
2020-06-12 15:59:53 +0300

    12 may - add index in Console-Project

--- Console-Project/script.js
+++ Console-Project/script.js
@@ -8,12 +8,41 @@ const personalMovieDB = {
     privat: false
 };
 
-const a = prompt('Один из последних просмотренных фильмов?', ''),
-      b = prompt('На сколько вы его оцените?', ''),
-      c = prompt('Один из последних просмотренных фильмов?', ''),
-      d = prompt('На сколько вы его оцените?', '');
+for (let i = 0; i < 2; i++){
+    const a = prompt('Один из последних просмотренных фильмов?', ''),
+          b = prompt('На сколько вы его оцените?', '');
 
-personalMovieDB.movies[a] = b;
-personalMovieDB.movies[c] = d;
+    if (a != null && b != null && a != '' && b != '' && a.length < 50){
+        personalMovieDB.movies[a] = b;
+    } else{
+        i--;
+    }    
+}
+
+
+//while
+// let i = 0;
+// while (i < 2){
+//     i++;
+//     const a = prompt('Один из последних просмотренных фильмов?', ''),
+//           b = prompt('На сколько вы его оцените?', '');
+
+//     if (a != null && b != null && a != '' && b != '' && a.length < 50){
+//             personalMovieDB.movies[a] = b;
+//     } else{
+//          i--;
+//     }    
+    
+// }
+
+if (personalMovieDB.count >= 10 & personalMovieDB.count < 30){
+    console.log("Вы классический зритель");
+}else if (personalMovieDB.count >= 30){
+    console.log("Вы киноман!");
+}else if(personalMovieDB.count < 10) {
+    console.log("Просмотренно довольно мало фильмов");
+}else{
+    console.log("Произошла ошибка");
+}
 
 console.log(personalMovieDB);
